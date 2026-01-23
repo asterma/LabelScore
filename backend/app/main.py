75d5054395e1363fe44c36b192a1c0c0cfd3fc68
SikunMaster <sikun.ma@liangdao.de>
2026-01-23 10:42:14 +0100

    local host change

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://0.0.0.0:3000",
+        "http://10.7.8.33:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
